c648f7efa5a53f3dd6c61dbc5f2c2a887e89165e
Derek Berner <derek.c.berner@vanderbilt.edu>
2013-10-11 16:22:58 -0500

    Removing work-related copyrights and stuff

--- src/main/java/com/bernerbits/fractal/colorer/HSColorer.java
+++ src/main/java/com/bernerbits/fractal/colorer/HSColorer.java
@@ -2,9 +2,9 @@
  *  File:         GrayscaleColorer.java
  *  Created:      Oct 11, 2013
  *  Last Changed: $Date$
- *  Author:       Derek Berner - derek.c.berner@vanderbilt.edu
+ *  Author:       Derek Berner - bernerbits@gmail.com
  *
- *  This code is copyright (c) 2013 Vanderbilt University Medical Center
+ *  
  */
 package com.bernerbits.fractal.colorer;
 
